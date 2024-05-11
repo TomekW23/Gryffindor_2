290f4057c9335cc66718b520d8e43b687aba5cff
Patryk Śliwiński <patryksliwinski2@gmail.com>
2024-05-11 12:08:35 +0200

    docs: Dodanie komentarzy

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 def waluta_str_na_dict(waluta_str: str) -> dict:
     ciag = waluta_str.split(" ")
+    
     galeon = 0
     sykl = 1
     knut = 0
 
     for element_index in range(len(ciag)):
+        # Sprawdź czy element to galeon jesli tak to poprzedni element jest wartością galeonu
         if ciag[element_index].startswith("g"):
             galeon = int(ciag[element_index-1])
             continue
+        # Sprawdź czy element to sykl jesli tak to poprzedni element jest wartością sykla
         if ciag[element_index].startswith("s"):
             sykl = int(ciag[element_index-1])
             continue
+        # Sprawdź czy element to knut jesli tak to poprzedni element jest wartością knuta
         if ciag[element_index].startswith("k"):
             knut = int(ciag[element_index-1])
             continue
