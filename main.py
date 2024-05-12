e8beb8e063b45b5e2089f6893cc80628785c5c1c
Lukasz <lukjar258@gmail.com>
2024-05-12 17:31:10 +0200

    Dodanie zadania 2

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+import random
+
+adresat = input("Podaj adresata listu:")
+tresc = input("Podaj treść listu:")
+
+def wyslij_sowe(adresat, tresc_listu):
+    print(f"Wysyłam sowe do {adresat} z treścią: {tresc_listu}")
+    time.sleep(1)
+    return random.choices([True, False], weights=(90, 10), k=1)[0]
+
+wysylam_list = wyslij_sowe(adresat, tresc)
+
+if wysylam_list:
+    print("Sowa została pomyślnie wysłana!")
+else:
+    print("Wysłanie sowy nie powiodło się.")
