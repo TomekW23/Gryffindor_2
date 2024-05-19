11a4b7f92765c93afc484e329e19ae4e0ae6200f
Lukasz <lukjar258@gmail.com>
2024-05-19 12:32:15 +0200

    Zadanie 2 v2

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 adresat = input("Podaj adresata listu:")
 tresc = input("Podaj treść listu:")
 def wyslij_sowe(adresat, tresc_listu):
-
     print(f"Wysyłam sowe do {adresat} z treścią: {tresc_listu}")
     time.sleep(1)
     return random.choices([True, False], weights=(90, 10), k=1)[0]
